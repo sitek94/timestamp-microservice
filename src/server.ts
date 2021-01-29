2a9454147d02fba32dfc2f6c2cc7de561323ef51
sitek94 <msitkowski94@gmail.com>
2021-01-29 15:47:28 +0100

    add timestamp api route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { prototype } from 'events';
+import { formatDate } from './utils';
 
 const app = express();
 
@@ -12,6 +14,42 @@ app.use(express.static('public'));
 // Basic route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+// Timestamp api route
+app.get('/api/timestamp/:date?', (req, res) => {
+  const date = req.params.date;
+  const timestamp = Number(date);
+  const dateObj = new Date(date);
+
+  // No parameter provided return values for current time
+  if (date === undefined) {
+    const now = new Date();
+
+    return res.json({
+      unix: now.getTime(),
+      utc: formatDate(now),
+    });
+  }
+
+  // User provided a number, treat it as timestamp
+  if (!isNaN(timestamp)) {
+    return res.json({
+      unix: timestamp,
+      utc: formatDate(new Date(timestamp)),
+    });
+  }
+
+  // Invalid format provided, return error
+  if (dateObj.toString() === 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+
+  // Correct format, return values for given date
+  return res.json({
+    unix: dateObj.getTime(),
+    utc: formatDate(dateObj),
+  });
 });
 
 const PORT = 5000;
