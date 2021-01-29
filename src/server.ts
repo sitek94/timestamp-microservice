7b18007ed2f69b48a38b7b5054f84633e0593ad7
sitek94 <msitkowski94@gmail.com>
2021-01-29 13:24:37 +0100

    serve basic html file

--- src/server.ts
+++ src/server.ts
@@ -8,3 +8,13 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 // Serve static files
 app.use(express.static('public'));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const PORT = 5000;
+
+// Start listening for requests
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
